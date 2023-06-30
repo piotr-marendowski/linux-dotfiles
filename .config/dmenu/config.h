b5913cf1e5a5ab744212e1a823ddf11fdd1ebcbd
piotr-marendowski <piotr-marendowski@tutanota.com>
2023-06-30 16:27:12 +0000

    Add dmenu config and xresources

--- .config/dmenu/config.h
+++ .config/dmenu/config.h
@@ -2,24 +2,26 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"JetBrainsMono Nerd Font:size=16"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+    [SchemeSelHighlight] = { "#24283b", "#a9b1d6" },
+	[SchemeNormHighlight] = { "#7aa2f7", "#24283b" },
 	[SchemeOut] = { "#000000", "#00ffff" },
-	[SchemeOutHighlight] = { "#ffc978", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 10;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 10;
+static unsigned int min_lineheight = 8;
 
 /*
  * Characters not considered part of a word while deleting words
